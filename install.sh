08d7ea100d837b155d6045d27fdb4865a82e4dbe
huajun <skatersecond@gmail.com>
2017-04-17 09:17:28 +0800

    Add permission check on install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 # install del
-cp delete.py /usr/local/lib
-cp lib/del >> /usr/local/bin && chmod +x /usr/local/bin/del
+
+if [ "$(whoami)" != 'root' ]
+then
+    echo "No permission"
+else
+    cp lib/delete.py /usr/local/lib
+    cp lib/del >> /usr/local/bin && chmod +x /usr/local/bin/del
+fi
+
 
