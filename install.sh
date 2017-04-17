7fc5b9f8fb8493fdbcf452fce7a29f9cfdaf90b6
huajun <skatersecond@gmail.com>
2017-04-17 09:30:34 +0800

    fix install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ then
     echo "No permission"
 else
     cp lib/delete.py /usr/local/lib
-    cp lib/del >> /usr/local/bin && chmod +x /usr/local/bin/del
+    cp lib/del /usr/local/bin && chmod +x /usr/local/bin/del
 fi
 
 
